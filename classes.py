a2e555b27fe89dee82276cde0725cf493c506d4f
bhoeflich <hoed>
2020-12-08 23:17:07 +0100

    added methods to get the value of x at y, to get the nst of polynomial, made a function to get the divisor

--- classes.py
+++ classes.py
@@ -1,14 +1,49 @@
+import functions
+
+
 class Polynomial:
-    def __init__(self, degree, coeffs):
-        self.degree = degree
-        self.coeffs = coeffs
+    def __init__(self, coeffs):
+        self.degree = len(coeffs)-1
+        self.coeffs = list(reversed(coeffs))
 
     def get_degree(self):
         return self.degree
 
     def get_coeffs(self):
-        return self.coeffs
+        return list(reversed(self.coeffs))
 
-p1 = Polynomial(5, [3, 4, 3 , 2, 1])
+    def has_nst(self):
+        return self.guess_nst() is not None
 
-print(p1.get_coeffs())
+    def get_y(self, x):
+        y = 0
+        for power, coeff in enumerate(self.coeffs):
+            y += coeff * pow(x, power)
+        return y
+        # https://treyhunner.com/2016/04/how-to-loop-with-indexes-in-python/
+        # enumerate macht liste mit indizes
+
+    def guess_nst(self):
+        divisors = functions.get_divisors(self.coeffs[0])
+        for divisor in divisors:
+            posVal = self.get_y(divisor)
+            if posVal == 0:
+                return divisor
+            negVal = self.get_y(divisor * -1)
+            if negVal == 0:
+                return divisor * -1
+        return None
+
+    def horner(self, nst):
+
+        inputvalid()
+
+p1 = Polynomial([3, 3, 2, 1, 5, 5])
+p2 = Polynomial([3, 4, 5, 6])
+p3 = Polynomial([1, -2, -3])
+p4 = Polynomial([1, -2.5, 1.5])
+print(p4.guess_nst())
+print(functions.get_divisors(-3))
+
+
+#print(p1.get_degree())
--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import numpy
+
 def inputvalid(txt):
     x = input(txt)
     if x.isdigit():
@@ -5,5 +7,14 @@ def inputvalid(txt):
     else:
         return inputvalid(f'Input {x} invalid, try again please ')
 
-# function die anzahl der coeffs mit grad vergleicht
+#findet ganzahlige Teiler einer Zahl
+def get_divisors(x):
+    divisors = []
+    abs_x = abs(x)
+    for p in numpy.arange(1, abs_x + 1, 0.1):
+        rounded_p = round(p*10)/10
+        if abs_x%rounded_p == 0:
+            divisors.append(rounded_p)
+    return divisors
+
 
